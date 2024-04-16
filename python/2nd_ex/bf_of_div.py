e00b2af067c18e85d819cf24bffc2cede08e674e
Alexei <leshamihaylenko@bk.ru>
2024-04-16 08:50:20 +0700

    miller test python

--- python/2nd_ex/bf_of_div.py
+++
@@ -1,7 +0,0 @@
-def brute_force_of_divisors(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
